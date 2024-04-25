8d82eb89ba37cdda815cdec083f9b52beab590d0
Tejaswini-Tiwari <tiwaritejaswini2601@gmail.com>
2024-04-25 13:49:30 +0530

    test1 & test2

--- program1.js
+++ program1.js
@@ -1,7 +1,29 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+    let longest = 0; 
+    let start = 0; 
+    let charIndexMap = {}; 
+    
+    for (let end = 0; end < s.length; end++) {
+        const currentChar = s[end];
+        
+        
+        if (charIndexMap[currentChar] >= start) {
+            start = charIndexMap[currentChar] + 1;
+        }
+        
+        
+        charIndexMap[currentChar] = end;
+        
+        
+        longest = Math.max(longest, end - start + 1);
+    }
+    
+    return longest;
+
 }
 
 module.exports = { longestSubstring };
+
 
 
--- program2.js
+++ program2.js
@@ -1,6 +1,18 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  const positiveNums = nums.filter(num => num > 0);
+
+    // Create a set to store positive integers
+    const numSet = new Set(positiveNums);
+
+    // Start checking from 1 and find the smallest positive integer missing in the set
+    let smallestMissing = 1;
+    while (numSet.has(smallestMissing)) {
+        smallestMissing++;
+    }
+
+    return smallestMissing;
 
 }
 
